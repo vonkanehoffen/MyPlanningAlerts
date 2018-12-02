bef84e956411eeeff66f9d2e0d761b6291658613
Kane Clover <kane@assure24.com>
2018-12-02 15:41:57 +0000

    Scrape all meta to object

--- scrapeTest/index.js
+++ scrapeTest/index.js
@@ -7,12 +7,23 @@ const readFile = promisify(fs.readFile)
 async function scrapeManchester() {
   const data = await readFile('./weeklyListResults.do.html', 'utf8')
 
+  let results = []
   const $ = cheerio.load(data)
-  const results = $('#searchresults .searchresult').each((i, r) => {
-    console.log($(r).find('a').first().text().trim())
+  $('#searchresults .searchresult').each((i, r) => {
+    // console.log('RESULT:::::')
+    // console.log($(r).html())
+    results.push({
+      title: $(r).find('a').first().text().trim(),
+      link: $(r).find('a').first().attr('href'),
+      address: $(r).find('.address').text().trim(),
+      openForComment: !!$(r).find('.canCommentIndicator').length,
+      ref:            $(r).find('.metaInfo').html().match(/Ref. No:([\s\S]*?)<span/)[1].trim(),
+      validatedDate:  $(r).find('.metaInfo').html().match(/Validated:([\s\S]*?)<span/)[1].trim(),
+      status:         $(r).find('.metaInfo').html().match(/Status:(.*)/s)[1].trim(),
+    })
   })
-
+  return results
 }
 
-scrapeManchester()
+scrapeManchester().then(r => console.log(r))
 
