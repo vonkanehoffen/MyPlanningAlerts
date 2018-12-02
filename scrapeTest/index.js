cad4f5de7ca6568d11ead6291c8f445559d78433
Kane Clover <kane@assure24.com>
2018-12-02 20:16:14 +0000

    Geocoding results

--- scrapeTest/index.js
+++ scrapeTest/index.js
@@ -1,6 +1,9 @@
 const cheerio = require('cheerio')
 const fs = require('fs')
 const { promisify } = require('util')
+const config = require('./config').default
+const fetch = require('node-fetch')
+const querystring = require('querystring');
 
 const readFile = promisify(fs.readFile)
 
@@ -10,9 +13,7 @@ async function scrapeManchester() {
   let results = []
   const $ = cheerio.load(data)
   $('#searchresults .searchresult').each((i, r) => {
-    // console.log('RESULT:::::')
-    // console.log($(r).html())
-    results.push({
+    const item = {
       title: $(r).find('a').first().text().trim(),
       link: $(r).find('a').first().attr('href'),
       address: $(r).find('.address').text().trim(),
@@ -20,10 +21,28 @@ async function scrapeManchester() {
       ref:            $(r).find('.metaInfo').html().match(/Ref. No:([\s\S]*?)<span/)[1].trim(),
       validatedDate:  $(r).find('.metaInfo').html().match(/Validated:([\s\S]*?)<span/)[1].trim(),
       status:         $(r).find('.metaInfo').html().match(/Status:(.*)/s)[1].trim(),
-    })
+    }
+    results.push(item)
   })
+
+  for(let i = 0; i < results.length; i++) {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?${querystring.stringify({
+      address: results[i].address,
+      key: config.geocodingAPIKey,
+    })}`)
+    const location = await response.json()
+// console.log(location)
+    if(location.status === 'OK') {
+      console.log('geocoded: ', location.results[0].formatted_address)
+      results[i].geometry = location.results[0].geometry
+    } else {
+      console.error('Geocode failure:', location)
+    }
+  }
+
   return results
 }
 
+// scrapeManchester()
 scrapeManchester().then(r => console.log(r))
 
