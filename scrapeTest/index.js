f14b041173536e244eeff53edff567db6a596f27
Kane Clover <kane@assure24.com>
2018-12-03 18:01:17 +0000

    Scrapes first page of live data for latest week

--- scrapeTest/index.js
+++ scrapeTest/index.js
@@ -4,16 +4,50 @@ const { promisify } = require('util')
 const config = require('./config').default
 const fetch = require('node-fetch')
 const querystring = require('querystring');
+const cookie = require('cookie');
+const { URLSearchParams } = require('url');
 
-const readFile = promisify(fs.readFile)
+// const readFile = promisify(fs.readFile)
+
+const urlRoot = 'https://pa.manchester.gov.uk'
 
 async function scrapeManchester() {
 
+  // 1. Get latest week
+  const searchForm = await fetch(`${urlRoot}/online-applications/search.do?action=weeklyList&searchType=Application`, {
+    headers: {
+      'User-Agent': config.userAgent,
+    }
+  })
 
-  const data = await readFile('./weeklyListResults.do.html', 'utf8')
+  const searchFormText = await searchForm.text()
+  const $searchForm = cheerio.load(searchFormText)
+  const latestDate = $searchForm('select#week option').first().attr('value')
+  const cookies = cookie.parse(searchForm.headers.get('set-cookie'))
+  console.log(latestDate, cookies)
+
+  // 2. Get page 1
+  const params = new URLSearchParams();
+
+  params.append('searchCriteria.ward', '');
+  params.append('week', latestDate);
+  params.append('dateType', 'DC_Validated');
+  params.append('searchType', 'Application');
+
+  const firstPage = await fetch(`${urlRoot}${$searchForm('form#weeklyListForm').attr('action')}`, {
+    method: 'POST',
+    headers: {
+      'User-Agent': config.userAgent,
+      'Cookie': cookie.serialize('JSESSIONID', cookies.JSESSIONID),
+    },
+    body: params,
+  })
+  const firstPageText = await firstPage.text()
+
+  // 3. Scrape data from results into object
 
   let results = []
-  const $ = cheerio.load(data)
+  const $ = cheerio.load(firstPageText)
   $('#searchresults .searchresult').each((i, r) => {
     const item = {
       title: $(r).find('a').first().text().trim(),
@@ -27,10 +61,12 @@ async function scrapeManchester() {
     results.push(item)
   })
 
+  // 4. Geocode all addresses
+
   for(let i = 0; i < results.length; i++) {
     const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?${querystring.stringify({
       address: results[i].address,
-      key: 'no' //config.geocodingAPIKey,
+      key: config.geocodingAPIKey,
     })}`)
     const location = await response.json()
     results[i].geocodeStatus = location.status
