8f8d5e1281d5dbc0828861f8edd9c1f771d6e39e
Kane Clover <kane@assure24.com>
2018-12-10 00:27:54 +0000

    Node 8 runtime running simple firestore op

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true })
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.runPlanningScrape = functions.https.onRequest(async (req, res) => {
+  const docRef = db.collection('planningApps').doc('alovelace');
+
+  const dbRes = await docRef.set({
+    first: 'Ada Again',
+    last: 'Lovelace',
+    born: 1815
+  });
+  res.send({stuff: 'this is the stuff', dbRes})
+  return null
+});
